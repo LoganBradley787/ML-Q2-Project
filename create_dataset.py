3602acb94ea8e88aac44f6091574c42464e39474
LoganBradley787 <loganbradley787@gmail.com>
2025-01-18 17:26:56 -0500

    replaced features with attributes

--- create_dataset.py
+++ create_dataset.py
@@ -3,20 +3,20 @@ from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-features = 10
+attributes = 10
 
 x_clusters, y_clusters = make_blobs(
     n_samples=[1000, 300, 300],
     centers=[
-        [0]*features,
-        [3]*features,
-        [-3]*features, 
+        [0]*attributes,
+        [3]*attributes,
+        [-3]*attributes, 
     ],
     cluster_std=[3, 7, 7],             
     random_state=1
 )
 
-x_noise = np.random.normal(loc=0, scale=10, size=(300, features)) # 300 instances of noise
+x_noise = np.random.normal(loc=0, scale=10, size=(300, attributes)) # 300 instances of noise
 y_noise = np.random.choice([0, 1, 2], size=300, p=[0.4, 0.3, 0.3]) # noise assigned to random class (40% probability of class 0, 30% probability of class 1, 30% probability of class 2)
 
 
